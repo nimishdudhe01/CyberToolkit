8b3a62c143ac91c2a54141100cd86d6e456d4ab5
Nimish Dudhe <20cs3043@rgipt.ac.in>
2024-04-11 06:23:33 +0530

    Fixed The Internal Server Error in Lookup and Updated Dockerfile.
    Implemented error handling for the case in which there was no associated string for the hash. And updated the Dockerfile.

--- CTLookup.py
+++ CTLookup.py
@@ -1,27 +1,32 @@
 from sqlalchemy import create_engine, text
 
 def reverse_lookup(hash_value):
-    # Create engine and session
-    engine = create_engine('sqlite:///hashes.sqlite')
-    conn = engine.connect()
+	# Create engine and session
+	engine = create_engine('sqlite:///hashes.sqlite')
+	conn = engine.connect()
 
-    if len(hash_value) == 32:
-        hashes = ['md4', 'md5']
-        result = conn.execute(text("SELECT string FROM hashes WHERE {} = :hash_value1 OR {} = :hash_value2".format(hashes[0], hashes[1])), {'hash_value1': hash_value, 'hash_value2': hash_value})
-    elif len(hash_value) == 40:
-        hashes = ['rmd160', 'sha1']
-        result = conn.execute(text("SELECT string FROM hashes WHERE {} = :hash_value1 OR {} = :hash_value2".format(hashes[0], hashes[1])), {'hash_value1': hash_value, 'hash_value2': hash_value})
-    elif len(hash_value) == 64:
-        hashes = ['sha256']
-        result = conn.execute(text("SELECT string FROM hashes WHERE {} = :hash_value".format(hashes[0])), {'hash_value': hash_value})
-    elif len(hash_value) == 128:
-        hashes = ['sha512']
-        result = conn.execute(text("SELECT string FROM hashes WHERE {} = :hash_value".format(hashes[0])), {'hash_value': hash_value})
+	result = None
 
-    row = result.fetchone()
+	if len(hash_value) == 32:
+		hashes = ['md4', 'md5']
+		result = conn.execute(text("SELECT string FROM hashes WHERE {} = :hash_value1 OR {} = :hash_value2".format(hashes[0], hashes[1])), {'hash_value1': hash_value, 'hash_value2': hash_value})
+	elif len(hash_value) == 40:
+		hashes = ['rmd160', 'sha1']
+		result = conn.execute(text("SELECT string FROM hashes WHERE {} = :hash_value1 OR {} = :hash_value2".format(hashes[0], hashes[1])), {'hash_value1': hash_value, 'hash_value2': hash_value})
+	elif len(hash_value) == 64:
+		hashes = ['sha256']
+		result = conn.execute(text("SELECT string FROM hashes WHERE {} = :hash_value".format(hashes[0])), {'hash_value': hash_value})
+	elif len(hash_value) == 128:
+		hashes = ['sha512']
+		result = conn.execute(text("SELECT string FROM hashes WHERE {} = :hash_value".format(hashes[0])), {'hash_value': hash_value})
 
-    # Close the connection
-    conn.close()
+	row = result.fetchone() if result is not None else None
 
-    # Return the result string or None if not found
-    return row[0] if row else None
+	#Close the connection
+	conn.close()
+
+	# Return the result string or None if not found
+	if row is not None and row[0] != '':
+		return row[0]
+	else:
+		return "Not Found!"
